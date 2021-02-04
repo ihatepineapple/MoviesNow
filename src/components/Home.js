00704e0927ae7318b01fe73c415329552bdee134
Irene A <ihatepineapple@gmail.com>
2021-02-04 17:31:38 +0100

    working on loading random movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,20 +6,58 @@ function Home() {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        axios.get(`http://www.omdbapi.com/?apikey=bfe43e50&i=tt3896198`)
-        .then(movieResults => setMovies(movieResults.data))
-        .catch(error => console.log(`${error}`))
+        getRandomMovies();
     }, []);
 
-        console.log(movies.length)
+        
 
+    const getRandomMovies = async () => {
+        
+        // let { data } = response.data
+        // setMovies(response.data)
+
+        // create new empty array
+        // define a random key value
+        // call axios with random variable on id
+        // push response into empty array until array .length is 8
+        let randomMoviesArray = [];
+        // let randomId = Math.floor(Math.random() * 8999999 + 1000000);
+
+            while (randomMoviesArray.length < 8) {
+            
+            for ( let i = 0; i < 8; i++ ) {
+                let randomId = Math.floor(Math.random() * 8999999 + 1000000);
+                let response = await axios.get(`http://www.omdbapi.com/?apikey=bfe43e50&i=tt${randomId}`);
+                let data  = response.data;
+                
+                if ( data.Response !== "False" && randomMoviesArray.length < 8) { 
+                    randomMoviesArray.push(data)};
+                
+            }
+        }
+
+        setMovies(randomMoviesArray)
+        return movies
+    }
+
+    
     return (
         <div>
             <NavBar /> 
 
-            <div>
+            {(movies.length === 8) ?
+                movies.map((movie, index) => {
+                    return(
+                        <div key={index}>
+                            <h1>{movie.Title}</h1>
+                        </div>
+                    )
+                })
+                :
+                <p>Loading movies, be patient</p>
+            }
 
-            </div>
+            
         </div>
     )
 }
